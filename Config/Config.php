c3fe03c0d61476935ab8a759ebafac176d925212
Jmartinezchu <jmartinezchu.03@gmail.com>
2024-01-08 10:11:43 -0500

    Hoteles - Usqay

---
+++ Config/Config.php
@@ -0,0 +1,33 @@
+<?php
+
+const BASE_URL = "/hoteles";
+
+date_default_timezone_set('America/Lima');
+
+
+const NAME_PROYECT = "Hoteles - Usqay";
+const RUC_EMPRESA  = "10204078901";
+const RAZON_SOCIAL = "";
+const TELEFONO_EMPRESA = 999134381;
+const EMAIL_EMPRESA = "ventas@sistemasusqay.com";
+const NOMBRE_EMPRESA = "Sistemas Usqay";
+const DIRECCION = "Piura - Piura";
+
+
+const DB_HOST = "localhost";
+const DB_NAME = "hoteles_peru";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "charset=utf8";
+
+// NUEVO IGV HASTA DICIEMBRE 8%
+const NEWIGV = 1.08;
+
+// IGV 18%
+const IGV = 1.18;
+
+const SPD = ",";
+const SPM = ".";
+
+const SMONEY = "S/";
+const SMONEY_DOLAR = "$";
